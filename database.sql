b9c5fc11006d27a1249e421b126705633d53379d
Tahmid L <103884318+tahlas@users.noreply.github.com>
2025-11-16 14:36:38 +0100

    Think we added all foreign keys

--- database.sql
+++ database.sql
@@ -1,15 +1,7 @@
-CREATE TABLE employee(
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    employment_id VARCHAR(500) UNIQUE,
-    salary INT,
-    supervisor/manager VARCHAR(500),
-    department_id INT GENERATED ALWAYS AS IDENTITY,
-    person_id INT GENERATED ALWAYS AS IDENTITY,
-    job_title_id INT GENERATED ALWAYS AS IDENTITY
-);
 
 CREATE TABLE person(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
     personal_number VARCHAR(12) UNIQUE,
     first_name VARCHAR(500),
     last_name VARCHAR(500),
@@ -21,30 +13,49 @@ CREATE TABLE person(
 
 CREATE TABLE department(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
     department_name VARCHAR(500),
-    manager VARCHAR(500) UNIQUE, 
+    manager VARCHAR(500) UNIQUE
 );
 
 CREATE TABLE job_title(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
     job_title VARCHAR(500) UNIQUE,
 );
 
+CREATE TABLE employee(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+    employment_id VARCHAR(500) UNIQUE,
+    salary INT,
+    manager VARCHAR(500),
+
+    department_id INT,
+    person_id INT,
+    job_title_id INT,
+
+    FOREIGN KEY (department_id) REFERENCES department(id),
+    FOREIGN KEY (person_id) REFERENCES person(id),
+    FOREIGN KEY (job_title_id) REFERENCES job_title(id)
+);
+
+CREATE TABLE skill(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+    skill_name VARCHAR(500),
+);
+
 CREATE TABLE employee_skill(
-    employee_id INTEGER NOT NULL,
-    skill_id INTEGER NOT NULL,
+    employee_id INT,
+    skill_id INT,
     PRIMARY KEY (employee_id, skill_id),
     FOREIGN KEY (employee_id) REFERENCES employee(id),
     FOREIGN KEY (skill_id) REFERENCES skill(id)   
 );
 
-CREATE TABLE skill(
-    id  INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    skill_name VARCHAR(500),
-);
-
 CREATE TABLE course_layout(
-    id  INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     course_code VARCHAR(10) UNIQUE,
     course_name VARCHAR(500),
     min_students INT,
@@ -55,24 +66,37 @@ CREATE TABLE course_layout(
 CREATE TABLE course_instance(
     instance_id VARCHAR(200) UNIQUE PRIMARY KEY,
     num_students INT,
-    course_layout_id INT GENERATED ALWAYS AS IDENTITY Foreign Key () REFERENCES (),
+    course_layout_id INT,
+    Foreign Key (course_layout_id) REFERENCES course_layout(id),
     study_period VARCHAR(10),
     study_year TIMESTAMP(4),
 );
 
 CREATE TABLE planned_activity(
-    /*Add foreign keys here!*/
+    PRIMARY KEY(teaching_activity_id, instance_id),
+    teaching_activity_id INT,
+    instance_id VARCHAR(200) UNIQUE,
+
     planned_hours INT
 );
 
 CREATE TABLE teaching_activity(
-    id  INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
     activity_name VARCHAR(500) UNIQUE,
     factor FLOAT(5)
 );
 
 CREATE TABLE employee_planed_activity(
-    /*Add foreign keys here!*/
+    PRIMARY KEY (employee_id, teaching_activity_id, instance_id),
+
+    employee_id INT,
+    teaching_activity_id INT,
+    instance_id VARCHAR(200),
+
+    FOREIGN KEY (employee_id) REFERENCES employee(id),
+    FOREIGN KEY (teaching_activity_id) REFERENCES teaching_activity(id),
+    FOREIGN KEY (instance_id) REFERENCES course_instance(instance_id)
 );
 
 
