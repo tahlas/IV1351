33cdffd56934d573035fc0a6435c07e2d90adbfa
Tahmid L <103884318+tahlas@users.noreply.github.com>
2025-11-26 15:06:01 +0100

    Joined all relevant tables for problem 1

--- Seminar_2/queries_v2.sql
+++ Seminar_2/queries_v2.sql
@@ -1,4 +1,5 @@
-SELECT course_name, course_code, hp, study_period, num_students FROM course_layout 
+SELECT course_name, course_code, hp, study_period, num_students 
+FROM course_layout 
 JOIN course_instance ON course_layout.id = course_instance.course_layout_id
 JOIN planned_activity ON course_instance.instance_id = planned_activity.instance_id
 JOIN teaching_activity ON planned_activity.teaching_activity_id = teaching_activity.id;
